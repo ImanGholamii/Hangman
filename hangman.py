8127d1a7c08625d4dcb484cb2eb7b5006d191e90
Iman <imangholamiimi@gmail.com>
2024-05-28 20:10:54 +0330

    :zap: | UPDATE | add pictures, show results | needs to improve logics...

--- hangman.py
+++ hangman.py
@@ -4,10 +4,11 @@ from random import choice
 print(logo)
 print(HANGMANPICS[0])
 
-# random_word = choice(word_list)
-random_word = 'abaaas'
+random_word = choice(word_list)
 blank_numbers = len(random_word)
 word = []
+results = ''
+
 for number in range(blank_numbers):
         word += '_'
         
@@ -28,22 +29,47 @@ else:
     print("⚠️  Invalid Input.\n🔄 Restart the game and enter '1' or 'easy' and so on...")
 
 
-
-        
-print(word)
-print(type(word))        
-print(random_word)
-w = 'a'
-
-for i in range(len(random_word)):
-    if random_word[i] == w:
-        word[i] = random_word[i]
         
 print(word)               
-    
-# while guess_remains >= 0:
-#     try:
-#        user_guess = str(input("Enter a word: "))
-#     except TypeError:
-#        print("Wrong Input!\nYou have to enter just words...") 
-    
+p = 1    
+while guess_remains > 0:
+    try:
+       user_guess = input("Enter a word: ")
+       if not user_guess.isalpha():
+           raise ValueError("You have to enter just words...")
+       
+    except ValueError as e:
+       print(f"Wrong Input!\n{e}")
+       if p >= len(HANGMANPICS):
+           print(HANGMANPICS[6])
+       else:
+           print(HANGMANPICS[p]) 
+       p += 1 
+    else:
+        print(f"Hint: it is a {len(random_word)} lenght word")
+        print(f"You entered {user_guess} ")
+        
+        if user_guess in random_word:
+            for i in range(len(random_word)):
+                
+                if random_word[i] == user_guess:
+                    word[i] = random_word[i]
+                    results += word[i]
+                    print(word)
+            
+        else:
+            if p >= len(HANGMANPICS):
+                print(HANGMANPICS[6])
+            else:
+                print(HANGMANPICS[p]) 
+            p += 1 
+                  
+                  
+    guess_remains -= 1
+    if guess_remains == 0 and results != random_word:
+        print(f"{HANGMANPICS[6]}\nHe is dead")
+        print(f"The word was {random_word}")
+    elif guess_remains == 0 and results == random_word:
+        print("You won.")    
+        
+print(results)    
